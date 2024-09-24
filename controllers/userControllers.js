bbae8331464c4fb6f233ebbb82233e05100a096f
Ratnaraj Anand <anand.ratnaraj@gmail.com>
2024-09-24 13:52:30 +0530

    all users related endpoint is done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,78 @@
+const User = require('../models/userModel')
 
-
-const getUserController = (req, res) => {
+const getUserController = async(req, res) => {
     try {
-        res.status(200).send('user data')
-        console.log(req.body.id)
+        const user =  await User.findById({_id:req.body.id})
+        if(!user){
+            return res.status(404).json({
+                status: "failed",
+                msg: "User not found"
+            })
+        }
+        
+        user.password = undefined;
+        res.status(200).json({
+            status:"success",
+            user
+        })
+
+
     } catch (error) {
-        res.status(500).send('error', error)
+        res.status(500).json({
+            status:"success",
+            msg: "Error in get user API"
+        })
     }
 }
 
+const updateUserController = async (req, res) => {
+   try {
+       const user = await User.findById({_id:req.body.id})
+       if(!user){
+         return res.status(404).json({
+            status:false,
+            msg: "User not found"
+         })
+       }
+       const {username, address,phone} = req.body
+       if(username) user.username = username;
+       if(address) user.address = address;
+       if(phone) user.phone = phone ;
+       await user.save()
+       res.status(200).json({
+        status: "success",
+        msg: "user updated"
+       })
 
-module.exports = getUserController
+   } catch (error) {
+       console.log(error)
+       res.status(500).json({
+        status:false,
+        msg: "error in update user api"
+       })
+
+   }
+}
+
+const deleteProfileController = async (req, res) => {
+    try {
+        await User.findByIdAndDelete({_id:req.params.id})
+        return res.status(200).json({
+            status:"success",
+            msg: "Your accound has been deleted"
+        })
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            status:false,
+            msg: "Error in delete profile api"
+        })
+    }
+}
+
+module.exports = {
+    getUserController,
+    updateUserController,
+    deleteProfileController
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const getUserController = require('../controllers/userControllers')
+const {getUserController, updateUserController,deleteProfileController} = require('../controllers/userControllers')
 const authMiddleware = require('../middlewares/authMiddleware')
 const router = express.Router()
 
 
 router.get('/getUser',authMiddleware, getUserController)
+router.put('/updateUser',authMiddleware, updateUserController )
+router.delete('/deleteUser/:id', authMiddleware, deleteProfileController)
+// forget password and reset password we can make it is easy
 
 module.exports = router
